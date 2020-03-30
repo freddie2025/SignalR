8abf18652be3b1138994eaec65c427ada926b55b
Fred James <fredjsam@live.co.uk>
2020-03-30 16:23:38 +0100

    SignalR Demo Complete

---
+++ SignalRDemo/Web/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+
+namespace Web
+{
+	[HubName("chat")]
+	public class ChatHub : Hub
+	{
+		[HubMethodName("announceToEverybody")]
+		public void Announce(string message)
+		{
+			Clients.All.Announce(message);
+		}
+
+		public DateTime GetServerDateTime()
+		{
+			return DateTime.Now;
+		}
+	}
+}
